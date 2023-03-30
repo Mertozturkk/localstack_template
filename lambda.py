7df228d92ef0cdc8beaa0318b532218a32239d0b
Mert Öztürk <mert.ozturk@teleskop.app>
2023-03-30 15:52:28 +0300

    deleted files

--- lambda.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-
-def lambda_handler(event, context):
-    logger.info('got event{}'.format(event))
-    return {'message': 'Hello World'}
